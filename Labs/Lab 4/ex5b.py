cd2d235b846ae68661c69ef389144ac1fc0a5448
Neysanfoo <neysanfoo@gmail.com>
2021-09-09 10:43:49 +0800

    Update ex5b.py

--- Labs/Lab 4/ex5b.py
+++ Labs/Lab 4/ex5b.py
@@ -9,59 +9,25 @@ Y = (255,255,0)
 G = (0,128,0)
 O = (0,0,0)
 
-def heart(col, orientation):
+def heart(col):
   C = col
-  if orientation == 0:
-    image = [
-    O, O, O, O, O, O, O, O, 
-    O, C, C, O, C, C, O, O,
-    C, C, C, C, C, C, C, O, 
-    C, C, C, C, C, C, C, O,
-    O, C, C, C, C, C, O, O,
-    O, O, C, C, C, O, O, O,
-    O, O, O, C, O, O, O, O,
-    O, O, O, O, O, O, O, O,
-    ]
-  if orientation == 1:
-    image = [
-    O, O, O, O, C, C, O, O, 
-    O, O, O, C, C, C, C, O,
-    O, O, C, C, C, C, C, O, 
-    O, C, C, C, C, C, O, O,
-    O, O, C, C, C, C, C, O,
-    O, O, O, C, C, C, C, O,
-    O, O, O, O, C, C, O, O,
-    O, O, O, O, O, O, O, O,
-    ]
-  if orientation == 2:
-    image = [
-    O, O, O, O, O, O, O, O, 
-    O, O, O, O, C, O, O, O,
-    O, O, O, C, C, C, O, O, 
-    O, O, C, C, C, C, C, O,
-    O, C, C, C, C, C, C, C,
-    O, C, C, C, C, C, C, C,
-    O, O, C, C, O, C, C, O,
-    O, O, O, O, O, O, O, O,
-    ]
-  if orientation == 3:
-    image = [
-    O, O, C, C, O, O, O, O, 
-    O, C, C, C, C, O, O, O,
-    O, C, C, C, C, C, O, O, 
-    O, O, C, C, C, C, C, O,
-    O, C, C, C, C, C, O, O,
-    O, C, C, C, C, O, O, O,
-    O, O, C, C, O, O, O, O,
-    O, O, O, O, O, O, O, O,
-    ]
+  image = [
+  O, O, O, O, O, O, O, O, 
+  O, C, C, O, C, C, O, O,
+  C, C, C, C, C, C, C, O, 
+  C, C, C, C, C, C, C, O,
+  O, C, C, C, C, C, O, O,
+  O, O, C, C, C, O, O, O,
+  O, O, O, C, O, O, O, O,
+  O, O, O, O, O, O, O, O,
+  ]
   return image
+  
 
 while True:
   color = [Y,G][randint(0,1)]
-  orientation = randint(0,3)
-  s.set_pixels(heart(color,orientation))
+  s.set_rotation([0,90,180,270][randint(0,3)])
+  s.set_pixels(heart(color))
   time.sleep(1)
   
   
-
